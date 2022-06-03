368de9d33a9edb0099357f6d7224bcd066d5c4ab
Miguel Álvarez <miguelwork92@gmail.com>
2022-06-03 22:23:16 +0200

    v0.11.0: update rustpotter and add noise detection options

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use rustpotter::{
-    DetectedWakeword, SampleFormat as RustpotterSampleFormat, WakewordDetector,
-    WakewordDetectorBuilder,
+    DetectedWakeword, NoiseDetectionMode as RustpotterNoiseDetectionMode,
+    SampleFormat as RustpotterSampleFormat, WakewordDetector, WakewordDetectorBuilder,
 };
 use wasm_bindgen::prelude::*;
 mod utils;
@@ -20,57 +20,57 @@ impl RustpotterJS {
     /// Loads a wakeword from its model bytes.
     pub fn addWakewordModelBytes(&mut self, data: Vec<u8>) {
         self.detector
-        .add_wakeword_from_model_bytes(data, true)
-        .unwrap();
+            .add_wakeword_from_model_bytes(data, true)
+            .unwrap();
     }
     /// Process i32 audio chunks.
-    /// 
+    ///
     /// Asserts that the audio chunk length should match the return
     /// of the get_samples_per_frame method.
-    /// 
+    ///
     /// Assumes sample rate match the configured for the detector.
-    /// 
+    ///
     /// Asserts that detector bits_per_sample is one of: 8, 16, 24, 32.
-    /// 
+    ///
     /// Asserts that detector sample_format is 'int'.
     pub fn processInt32(&mut self, buffer: &[i32]) -> Option<RustpotterDetection> {
         transform_detection(self.detector.process_i32(buffer))
     }
     /// Process i16 audio chunks.
-    /// 
+    ///
     /// Asserts that the audio chunk length should match the return
     /// of the get_samples_per_frame method.
-    /// 
+    ///
     /// Assumes sample rate match the configured for the detector.
-    /// 
+    ///
     /// Asserts that detector bits_per_sample is one of: 8, 16.
-    /// 
+    ///
     /// Asserts that detector sample_format is 'int'.
     pub fn processInt16(&mut self, buffer: &[i16]) -> Option<RustpotterDetection> {
         transform_detection(self.detector.process_i16(buffer))
     }
     /// Process i8 audio chunks.
-    /// 
+    ///
     /// Asserts that the audio chunk length should match the return
     /// of the get_samples_per_frame method.
-    /// 
+    ///
     /// Assumes sample rate match the configured for the detector.
-    /// 
+    ///
     /// Asserts that detector bits_per_sample is 8.
-    /// 
+    ///
     /// Asserts that detector sample_format is 'int'.
     pub fn processInt8(&mut self, buffer: &[i8]) -> Option<RustpotterDetection> {
         transform_detection(self.detector.process_i8(buffer))
     }
     /// Process f32 audio chunks.
-    /// 
+    ///
     /// Asserts that the audio chunk length should match the return
     /// of the get_samples_per_frame method.
-    /// 
+    ///
     /// Assumes sample rate match the configured for the detector.
-    /// 
+    ///
     /// Asserts that detector bits_per_sample is 32.
-    /// 
+    ///
     /// Asserts that detector sample_format is 'float'.
     pub fn processFloat32(&mut self, buffer: &[f32]) -> Option<RustpotterDetection> {
         transform_detection(self.detector.process_f32(buffer))
@@ -83,7 +83,7 @@ impl RustpotterJS {
 fn transform_detection(detection_option: Option<DetectedWakeword>) -> Option<RustpotterDetection> {
     if detection_option.is_some() {
         let result = detection_option.unwrap();
-        Some(RustpotterDetection{
+        Some(RustpotterDetection {
             name: result.wakeword.clone(),
             score: result.score,
         })
@@ -197,11 +197,52 @@ impl RustpotterJSBuilder {
     pub fn setSingleThread(&mut self, value: bool) {
         self.builder.set_single_thread(value);
     }
+    ///
+    /// Defaults to 3.
+    ///
+    /// Only applies if noise mode is set, if 0 noise detection is not disabled after a detection.
+    pub fn setNoiseDelay(&mut self, value: u16) {
+        self.builder.set_noise_delay(value);
+    }
+    /// Noise/silence ratio in the last second to consider voice detected.
+    ///
+    /// Defaults to 0.5.
+    ///
+    /// Only applies if noise mode is set.
+    pub fn setNoiseSensitivity(&mut self, value: f32) {
+        self.builder.set_noise_sensitivity(value);
+    }
+    /// Use build-in noise detection to reduce computation on absence of noise.
+    ///
+    /// Configures how difficult is to considering a frame as noise (the required noise lever).
+    ///
+    /// Unless specified the noise detection is disabled.
+    pub fn setNoiseMode(&mut self, value: NoiseDetectionMode) {
+        self.builder.set_noise_mode(transform_noise_mode(value));
+    }
     /// construct the wakeword detector
     pub fn build(&self) -> RustpotterJS {
         RustpotterJS {
             detector: self.builder.build(),
         }
+    }
+}
+#[wasm_bindgen]
+#[allow(non_camel_case_types)]
+pub enum NoiseDetectionMode {
+    easiest,
+    easy,
+    normal,
+    hard,
+    hardest,
+}
+fn transform_noise_mode(mode: NoiseDetectionMode) -> RustpotterNoiseDetectionMode {
+    match mode {
+        NoiseDetectionMode::easiest => RustpotterNoiseDetectionMode::Easiest,
+        NoiseDetectionMode::easy => RustpotterNoiseDetectionMode::Easy,
+        NoiseDetectionMode::normal => RustpotterNoiseDetectionMode::Normal,
+        NoiseDetectionMode::hard => RustpotterNoiseDetectionMode::Hard,
+        NoiseDetectionMode::hardest => RustpotterNoiseDetectionMode::Hardest,
     }
 }
 #[wasm_bindgen]
--- tools/build-web.sh
+++ tools/build-web.sh
@@ -3,3 +3,4 @@ set -e
 rm -rf pkg
 wasm-pack build --target web --scope web
 sed -i 's/@web\/rustpotter-wasm/rustpotter-web/g' pkg/package.json
+(cd pkg && npm publish)
