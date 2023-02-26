d3bc82e83dcc0d9e09b20bf774a584f9dc362b04
Miguel Álvarez <miguelwork92@gmail.com>
2023-02-26 21:13:04 +0100

    start v2

--- tools/build-web-slim.sh
+++ tools/build-web-slim.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 rm -rf pkg
-wasm-pack build --release --target web --no-default-features --scope web
+wasm-pack build --release --target web --scope web --no-default-features
 sed -i 's/@web\/rustpotter-wasm/rustpotter-web-slim/g' pkg/package.json
