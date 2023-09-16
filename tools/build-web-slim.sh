cdc93eabfe23bfe598fa13dc3dd97e27e3d827fa
Miguel Álvarez <miguelwork92@gmail.com>
2023-09-16 18:00:37 +0200

    add missing module type to libraries

--- tools/build-web-slim.sh
+++ tools/build-web-slim.sh
@@ -2,4 +2,5 @@
 set -e
 rm -rf pkg
 wasm-pack build --release --target web --scope web --no-default-features
+(cd pkg && npm pkg set type='module')
 sed -i 's/@web\/rustpotter-wasm/rustpotter-web-slim/g' pkg/package.json
--- tools/build-web.sh
+++ tools/build-web.sh
@@ -2,4 +2,5 @@
 set -e
 rm -rf pkg
 wasm-pack build --release --target web --scope web
+(cd pkg && npm pkg set type='module')
 sed -i 's/@web\/rustpotter-wasm/rustpotter-web/g' pkg/package.json
